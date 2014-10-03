8e80787425a6c6d747a82384297adc9af9248d95
Michael Greenhill <doctorjbeam@gmail.com>
2014-10-03 18:01:05 +1000

    Added Liveries core code
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

---
+++ lib/Locos/Liveries/Base.php
@@ -0,0 +1,79 @@
+<?php
+	/** 
+	 * Loco liveries
+	 * @since Version 3.2
+	 * @package Railpage
+	 * @author Michael Greenhill
+	 */
+	
+	namespace Railpage\Locos\Liveries; 
+	
+	use Railpage\AppCore;
+	use Exception;
+	
+	/**
+	 * Base loco liveries class
+	 * @since Version 3.2
+	 */
+	
+	class Base extends AppCore {
+		
+		/**
+		 * Return a list of livery IDs
+		 * @since Version 3.2
+		 * @return array
+		 */
+		
+		public function listAll() {
+			$query = "SELECT livery_id FROM loco_livery ORDER BY livery";
+			
+			$return = array(); 
+			
+			foreach ($this->db->fetchAll($query) as $row) {
+				$return[] = $row['livery_id']; 
+			}
+			
+			return $return;
+		}
+		
+		/**
+		 * Return a list of livery IDs
+		 * @since Version 3.7.5
+		 * @return array
+		 */
+		
+		public function listAllFull() {
+			$query = "SELECT * FROM loco_livery ORDER BY livery";
+			
+			$return = array();
+			
+			foreach ($this->db->fetchAll($query) as $row) {
+				$Livery = new Livery($row['livery_id']);
+				$row['url'] = $Livery->url;
+					
+				$return[] = $row;
+			}
+			
+			return $return;
+		}
+		
+		/**
+		 * Find liveries from Flickr tags
+		 * @since Version 3.2
+		 * @param array $rawtags
+		 * @return array
+		 */
+		
+		public function findFromTag($rawtags = false) {
+			$tags = array();
+			
+			foreach ($rawtags as $tag) {
+				if (preg_match("@railpage\:livery\=([0-9]+)@", $tag, $matches)) {
+					$tags[] = $matches[1];
+				}
+			}
+			
+			return $tags;
+		}
+	}
+?>
---
+++ lib/Locos/Liveries/Livery.php
@@ -0,0 +1,267 @@
+<?php
+	/** 
+	 * Livery class
+	 * @since Version 3.2
+	 * @package Railpage
+	 * @author Michael Greenhill
+	 */
+	
+	namespace Railpage\Locos\Liveries;
+	
+	use Exception;
+	use Railpage\Images\Images;
+	
+	/**
+	 * Livery class
+	 * @since Version 3.2
+	 */
+	
+	class Livery extends Base {
+		
+		/**
+		 * Livery ID
+		 * @since Version 3.2
+		 * @var int $id
+		 */
+		
+		public $id;
+		
+		/**
+		 * Livery name
+		 * @since Version 3.2
+		 * @var string $name
+		 */
+		
+		public $name;
+		
+		/**
+		 * Date introduced
+		 * @since Version 3.2
+		 * @var string $introduced
+		 */
+		
+		public $introduced;
+		
+		/**
+		 * Date phased out
+		 * @since Version 3.2
+		 * @var string $withdrawn
+		 */
+		
+		public $withdrawn;
+		
+		/** 
+		 * This livery was superseded by x
+		 * @since Version 3.2
+		 * @var int $superseded_by
+		 */
+		
+		public $superseded_by;
+		
+		/** 
+		 * This livery was superseded by x ID
+		 * @since Version 3.2
+		 * @var int $superseded_by_id
+		 */
+		
+		public $superseded_by_id;
+		
+		/**
+		 * This livery supersedes x
+		 * @since Version 3.2
+		 * @var int $supersedes
+		 */
+		
+		public $supersedes;
+		
+		/**
+		 * This livery supersedes x ID
+		 * @since Version 3.2
+		 * @var int $supersedes_id
+		 */
+		
+		public $supersedes_id;
+		
+		/**
+		 * Flickr photo ID
+		 * @since Version 3.2
+		 * @var string $photo
+		 */
+		
+		public $photo_id;
+		
+		/**
+		 * Region within a country this livery is primarily found in. 
+		 * For inter-region (inter-state) liveries, leave blank
+		 *
+		 * @since Version 3.2
+		 * @var string $region
+		 */
+		
+		public $region;
+		
+		/**
+		 * Country this livery is primarily found in
+		 * For international liveries, leave blank
+		 *
+		 * @since Version 3.2
+		 * @var string $country
+		 */
+		
+		public $country;
+		
+		/**
+		 * Do we recurse through child objects and populate them? Could be very very memory intensive
+		 * @since Version 3.2
+		 * @var boolean $recurse
+		 */
+		
+		public $recurse;
+		
+		/**
+		 * Flickr machine tag
+		 * @since Version 3.8.7
+		 * @var string $tag
+		 */
+		
+		public $tag;
+		
+		/**
+		 * Loco image object
+		 * @since Version 3.8.7
+		 * @var \Railpage\Images\Image $Image
+		 */
+		
+		public $Image;
+		
+		/**
+		 * Constructor
+		 * @since Version 3.2
+		 * @param int $id
+		 * @param boolean $recurse
+		 */
+		
+		public function __construct($id, $recurse = true) {
+			parent::__construct();
+			
+			$this->namespace = "railpage.locos.liveries.livery";
+			
+			// Fetch any child objects
+			$this->recurse = $recurse;
+			
+			if ($id) {
+				$this->id = $id;
+				
+				$this->url = "/flickr?tag=railpage:livery=" . $this->id;
+				
+				try {
+					$this->fetch();
+				} catch (Exception $e) {
+					throw new Exception($e->getMessage()); 
+				}
+			}
+		}
+		
+		/**
+		 * Load this livery object
+		 * @since Version 3.2
+		 * @return boolean
+		 */
+		
+		public function fetch() {
+			if (!filter_var($this->id, FILTER_VALIDATE_INT)) {
+				throw new Exception("Cannot fetch Livery object - no ID given"); 
+				return false;
+			}
+			
+			$query = "SELECT * FROM loco_livery WHERE livery_id = ?";
+			$row = $this->db->fetchRow($query, $this->id); 
+			
+			if (isset($row) && is_array($row)) {		
+				$this->name 		= $row['livery'];
+				$this->introduced	= $row['introduced'];
+				$this->withdrawn	= $row['withdrawn'];
+				$this->superseded_by_id	= ($row['superseded_by'] > 0) ? $row['superseded_by'] : NULL;
+				$this->supersedes_id	= ($row['supersedes'] > 0) ? $row['supersedes'] : NULL;
+				$this->photo_id			= $row['photo_id'];
+				$this->region		= $row['region'];
+				$this->country		= $row['country'];
+				$this->tag = sprintf("railpage:livery=%d", $this->id);
+				$this->url = "/flickr?tag=" . $this->tag;
+				
+				if (filter_var($this->photo_id, FILTER_VALIDATE_INT)) {
+					$Images = new Images;
+					$this->Image = $Images->findImage("flickr", $this->photo_id);
+				}
+				
+				if ($this->recurse && $this->superseded_by_id > 0) {
+					$Superseded_by = new Livery($this->superseded_by_id, false); 
+					$this->superseded_by = $Superseded_by->name;
+				}
+				
+				if ($this->recurse && $this->supersedes_id > 0) {
+					$Supersedes = new Livery($this->supersedes_id, false); 
+					$this->supersedes = $Supersedes->name;
+				}
+				
+				return true;
+			} else {
+				throw new Exception("Cannot fetch Livery object - no livery for ID ".$this->id." was found");
+				return false;
+			}
+		}
+		
+		/**
+		 * Commit changes to this livery
+		 * @return boolean
+		 */
+		
+		public function commit() {
+			$this->validate(); 
+			
+			$data = array(
+				"livery" => $this->name,
+				"introduced" => $this->introduced,
+				"withdrawn" => $this->withdrawn,
+				"superseded_by" => $this->superseded_by_id,
+				"supersedes" => $this->supersedes_id,
+				"photo_id" => $this->photo_id,
+				"region" => $this->region,
+				"country" => $this->country
+			);
+			
+			if (empty($this->id)) {
+				$this->db->insert("loco_livery", $data); 
+				$this->id = $this->db->lastInsertId();
+			} else {
+				$where = array(
+					"livery_id = ?" => $this->id
+				);
+				
+				$this->db->update("loco_livery", $data, $where);
+			}
+			
+			
+			return true;
+		}
+		
+		/**
+		 * Validate this livery
+		 * @return true
+		 * @throws \Exception if $this->name is empty
+		 * @throws \Exception if $this->country is empty
+		 */
+		
+		public function validate() {
+			if (empty($this->name)) {
+				throw new Exception("Cannot validate changes to this livery - livery name cannot be empty"); 
+			}
+			
+			if (empty($this->country)) {
+				throw new Exception("Cannot validate changes to this livery - country cannot be empty"); 
+			}
+			
+			return true;
+		}
+	}
+?>
