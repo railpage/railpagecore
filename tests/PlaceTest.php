191bb6d82d3d9c17d126c90b547ba492b37b1556
Michael Greenhill <doctorjbeam@gmail.com>
2014-10-10 13:59:58 +1100

    More unit tests for \Railpage\Place
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- tests/PlaceTest.php
+++ tests/PlaceTest.php
@@ -12,6 +12,28 @@
 			$this->assertEquals($lat, $Place->lat);
 			$this->assertEquals($lon, $Place->lon);
 			$this->assertInstanceOf("Railpage\\Locations\\Country", $Place->Country);
+			$this->assertInstanceOf("Railpage\\Locations\\Region", $Place->Region);
+			$this->assertInternalType("float", $Place->boundingBox->southWest->lat);
+		}
+		
+		public function testGetAddress() {
+			$lat = "-37.13009600";
+			$lon = "145.07711000";
+			
+			$Place = new Place($lat, $lon);
+			
+			$this->assertInternalType("array", $Place->getAddress());
+		}
+		
+		public function testGetWeather() {
+			$lat = "-37.13009600";
+			$lon = "145.07711000";
+			
+			$Place = new Place($lat, $lon);
+			
+			$weather = $Place->getWeatherForecast();
+			$this->assertInternalType("array", $weather);
+			$this->assertCount(14, $weather);
 		}
 	}
 ?>
