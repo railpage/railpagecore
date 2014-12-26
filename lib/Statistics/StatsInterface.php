40ba9128d7fadb408f998d810800e0cb9e4c3e24
Michael Greenhill <doctorjbeam@gmail.com>
2014-12-26 21:45:48 +1100

    Added Statistics
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

---
+++ lib/Statistics/StatsInterface.php
@@ -0,0 +1,28 @@
+<?php
+	/**
+	 * Site statistics interface
+	 * @since Version 3.8.7
+	 * @package Railpage
+	 * @author Michael Greenhill
+	 */
+	
+	namespace Railpage\Statistics;
+	
+	/**
+	 * Stats interface
+	 * @since Version 3.8.7
+	 */
+	
+	interface StatsInterface {
+		
+		/**
+		 * Get a statistic
+		 * @param int $stat_id
+		 * @return array
+		 */
+		
+		public function getStatistic($stat_id); 
+		
+		
+	}
+?>
