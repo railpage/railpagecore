06612865e2ed9cb8110bb2e52e6cac906bca950d
Michael Greenhill <doctorjbeam@gmail.com>
2014-12-26 21:45:38 +1100

    Added SiteMessage
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

---
+++ lib/SiteMessage/Admin.php
@@ -0,0 +1,93 @@
+<?php
+	/**
+	 * Messages class
+	 * @author Michael Greenhill
+	 * @package Railpage
+	 * @copyright Copyright (c) 2011, Michael Greenhill
+	 * @since Version 3.0
+	 */
+	 
+	namespace Railpage\SiteMessage;
+	
+	use Exception;
+	use DateTime;
+
+	/** 
+	 * Administration module
+	 * @since Version 3.0
+	 */
+	
+	class Admin extends SiteMessage {
+		
+		/**
+		 *
+		 * Return a list of all messages
+		 * @since Version 3.0
+		 * @version 3.0
+		 * @param int $start 
+		 * @param int $perpage 
+		 * @return mixed
+		 *
+		 */
+		
+		public function listMessages($start = 0, $perpage = 25) {
+			if ($this->db instanceof \sql_db) {
+				$query = "SELECT * FROM messages ORDER BY message_id LIMIT ".$start.", ".$perpage."";
+				
+				if ($rs = $this->db->query($query)) {
+					$return = array(); 
+					
+					while ($row = $rs->fetch_assoc()) {
+						$return[] = $row; 
+					}
+					
+					return $return;
+				} else {
+					trigger_error("Messages: Unable to retrieve list of messages");
+					trigger_error($this->db->error); 
+					trigger_error($query); 
+					return false;
+				}
+			} else {
+				return $this->db->fetchAll("SELECT * FROM messages ORDER BY message_id LIMIT ?, ?", array($start, $perpage));
+			}
+		}
+		
+		
+		/**
+		 *
+		 * Create a new message
+		 * @since Version 3.0.1
+		 * @version 3.0
+		 * @param string $message_text
+		 * @return mixed
+		 *
+		 */
+		 
+		public function add($message_text = false) {
+			if (!$message_text) {
+				return false;
+			}
+			
+			if ($this->db instanceof \sql_db) {
+				$query = "INSERT INTO messages (message_text) VALUES ('".$this->db->real_escape_string($message_text)."')";
+				
+				if ($rs = $this->db->query($query)) {
+					return $db->insert_id; 
+				} else {
+					trigger_error("Messages: could not add new message"); 
+					trigger_error($this->db->error); 
+					trigger_error($query); 
+					return false;
+				}
+			} else {
+				$data = array(
+					"message_text" => $message_text
+				);
+				
+				if ($this->db->insert("messages", $data)) {
+					return $this->db->lastInsertId(); 
+				}
+			}
+		}
+	}
---
+++ lib/SiteMessage/SiteMessage.php
@@ -0,0 +1,103 @@
+<?php
+	/**
+	 * Messages class
+	 * @author Michael Greenhill
+	 * @package Railpage
+	 * @copyright Copyright (c) 2011, Michael Greenhill
+	 * @since Version 3.0
+	 */
+	 
+	namespace Railpage\SiteMessage;
+	
+	use Railpage\AppCore;
+	use Exception;
+	use DateTime;
+	
+	class SiteMessage extends AppCore {
+		
+		/** 
+		 * Message ID
+		 * @since Version 3.0
+		 * @version 3.0
+		 * @var int
+		 */
+		 
+		public $id;
+		
+		/**
+		 *
+		 * Constructor
+		 * @param object $db 
+		 * @param int $db
+		 * @since Version 3.0
+		 * @version 3.0
+		 *
+		 */
+		 
+		public function __construct() {
+			parent::__construct();
+			
+			foreach (func_get_args() as $arg) {
+				if (filter_var($arg, FILTER_VALIDATE_INT)) {
+					$this->id = $arg;
+				}
+			}
+		}
+		
+		
+		/**
+		 *
+		 * Display the latest message
+		 * @since Version 3.0
+		 * @param int $user_id
+		 * @param boolean $fullArray
+		 * @return mixed
+		 *
+		 */
+		 
+		public function newest($user_id = false, $fullArray = false) {
+			if ($this->db instanceof \sql_db) {
+				if ($user_id) {
+					// Logged in, have they dismissed this message?
+					$query = "SELECT * FROM messages WHERE message_active = 1 AND message_id NOT IN (SELECT message_id FROM messages_viewed WHERE user_id = ".$this->db->real_escape_string($user_id).") ORDER BY message_id DESC LIMIT 1";
+				} else {
+					// Guest, show it
+					$query = "SELECT * FROM messages WHERE message_active = 1 ORDER BY message_id DESC LIMIT 1";
+				}
+				
+				if ($rs = $this->db->query($query)) {
+					if ($rs->num_rows == 1) {
+						$row = $rs->fetch_assoc(); 
+						
+						if ($fullArray) {
+							return $row; 
+						} else {
+							return $row['message_text']; 
+						}
+					} else {
+						return false;
+					}
+				} else {
+					trigger_error("Messages: Unable to retrieve latest message");
+					trigger_error($this->db->error); 
+					trigger_error($query); 
+					return false;
+				}
+			} else {
+				if ($user_id) {
+					// Logged in, have they dismissed this message?
+					$query = "SELECT * FROM messages WHERE message_active = 1 AND message_id NOT IN (SELECT message_id FROM messages_viewed WHERE user_id = ?) ORDER BY message_id DESC LIMIT 1";
+					
+					$result = $this->db->fetchRow($query, $user_id);
+					return $fullArray ? $result : $result['message_text'];
+				} else {
+					// Guest, show it
+					$query = "SELECT * FROM messages WHERE message_active = 1 ORDER BY message_id DESC LIMIT 1";
+					
+					$result = $this->db->fetchRow($query);
+					return $fullArray ? $result : $result['message_text'];
+				}
+			}
+		}
+	}
+?>
