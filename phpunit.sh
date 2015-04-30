283364c2a84b23449dbf1ebfd615152f82bf27aa
Michael Greenhill <doctorjbeam@gmail.com>
2015-04-30 23:00:14 +1000

    Nicer looking PHPUnit script
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- phpunit.sh
+++ phpunit.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
-echo Dropping old database
+echo ""
+echo "================================================================"
+echo ""
+echo "   -- PHPUnit tests for Railpage --"
+echo ""
+echo "Setting up database:"
+echo " - Dropping old database"
 mysql -h localhost -e 'drop database sparta_unittest;'
-echo Importing database structure
+echo " - Importing database structure"
 mysql -h localhost < tests/data/travis/db.structure.sql
-echo Running phpUnit ::
-/usr/local/bin/phpunit
+echo ""
+echo "Running phpUnit ::"
+time /usr/local/bin/phpunit
+times
+
+if [ -e /usr/local/php7/bin/php ]; then
+	echo ""
+	echo ""
+    echo "   -- Resetting test environment for php7 --"
+	echo ""
+	echo ""
+
+    echo "Setting up database:"
+    echo " - Dropping old database"
+    mysql -h localhost -e 'drop database sparta_unittest;'
+    echo " - Importing database structure"
+    mysql -h localhost < tests/data/travis/db.structure.sql
+	echo ""
+    echo "Running phpUnit with php7 ::"
+    
+    time /usr/local/php7/bin/php /usr/local/bin/phpunit
+fi
+
+echo ""
+echo "================================================================"
+echo ""
