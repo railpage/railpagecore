50f3b949aa2429dd822071146acd4f635f45b21c
Michael Greenhill <doctorjbeam@gmail.com>
2015-04-30 17:43:44 +1000

    Moved PHPUnit/Travis-CI db structure file
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- phpunit.sh
+++ phpunit.sh
@@ -3,6 +3,6 @@
 echo Dropping old database
 mysql -h localhost -e 'drop database sparta_unittest;'
 echo Importing database structure
-mysql -h localhost < db.dist/db.structure.travis.sql
+mysql -h localhost < tests/data/travis/db.structure.sql
 echo Running phpUnit ::
 /usr/local/bin/phpunit
