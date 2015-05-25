a369b3a60354f0d8b64887a938b29d37a386bcb2
Michael Greenhill <doctorjbeam@gmail.com>
2015-05-25 11:39:54 +1000

    Input filtering
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- lib/Forums/Forums.php
+++ lib/Forums/Forums.php
@@ -1071,7 +1071,7 @@
 			 */
 			
 			if (isset($_COOKIE[$cookiename])) {
-				$cookiedata = $_COOKIE[$cookiename];
+				$cookiedata = filter_input(INPUT_COOKIE, $cookiename); #$_COOKIE[$cookiename];
 				$data = self::unserializeArray($cookiedata);
 				
 				if (count($data)) {
