555aadab22a6efa9e1575ca0a6692041edb5d1b7
Michael Greenhill <doctorjbeam@gmail.com>
2015-05-26 22:10:21 +1000

    Ideas test updates
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- tests/Railpage/Ideas/IdeasTest.php
+++ tests/Railpage/Ideas/IdeasTest.php
@@ -21,9 +21,15 @@
 			
 			$this->assertFalse(!filter_var($Category->id, FILTER_VALIDATE_INT)); 
 			
+			return $Category->id;
+			
 		}
 		
-		public function testAddIdea() {
+		/**
+		 * @depends testAddCategory
+		 */
+		
+		public function testAddIdea($category_id) {
 			
 			$User = new User;
 			$User->username = "Test12123";
@@ -33,18 +39,30 @@
 			$this->author_id = $User->id;
 			
 			$Idea = new Idea;
-			$Idea->Category = new Category(1);
+			$Idea->Category = new Category($category_id);
 			$Idea->title = self::IDEA_TITLE;
 			$Idea->description = self::IDEA_DESC;
 			$Idea->setAuthor($User)->commit(); 
 			
 			$this->assertFalse(!filter_var($Idea->id, FILTER_VALIDATE_INT)); 
 			
+			$idea_id = $Idea->id;
+			
+			$Idea = new Idea($idea_id);
+			$this->assertEquals($idea_id, $Idea->id); 
+			$this->assertEquals(self::IDEA_TITLE, $Idea->title);
+			
+			return $idea_id;
+			
 		}
 		
-		public function testCompareIdea() {
+		/**
+		 * @depends testAddIdea
+		 */
+		
+		public function testCompareIdea($idea_id) {
 			
-			$Idea = new Idea(1); 
+			$Idea = new Idea($idea_id); 
 			
 			$this->assertEquals(self::IDEA_TITLE, $Idea->title); 
 			$this->assertEquals(self::IDEA_DESC, $Idea->description);
@@ -56,10 +74,14 @@
 			
 		}
 		
-		public function testCanVote() {
+		/**
+		 * @depends testAddIdea
+		 */
+		
+		public function testCanVote($idea_id) {
 			
 			$User = new User($this->author_id);
-			$Idea = new Idea(1);
+			$Idea = new Idea($idea_id);
 			
 			$this->assertFalse($Idea->canVote($User));
 			
@@ -78,6 +100,34 @@
 			$this->assertFalse($Idea->canVote($User)); 
 			$this->assertEquals(1, count($Idea->getVoters()));
 			
+		}
+		
+		/**
+		 * @depends testAddCategory
+		 */
+		
+		public function testGetCategories() {
+			
+			$Ideas = new Ideas;
+			
+			foreach ($Ideas->getCategories() as $Category) {
+				$this->assertEquals(self::CAT_TITLE, $Category->name);
+				$this->assertFalse(!filter_var($Category->id, FILTER_VALIDATE_INT));
+			}
+		}
+		
+		/**
+		 * @depends testAddIdea
+		 */
+		
+		public function testGetNewIdeas() {
+			
+			$Ideas = new Ideas;
+			
+			foreach ($Ideas->getNewIdeas() as $Idea) {
+				$this->assertFalse(!filter_var($Idea->id, FILTER_VALIDATE_INT)); 
+				
+			}
 		}
 	}
 	
