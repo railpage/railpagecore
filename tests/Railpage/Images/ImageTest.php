6d8bf681d05be41e70dd73d9175b8f34072cda35
Michael Greenhill <doctorjbeam@gmail.com>
2015-05-26 22:10:36 +1000

    Added images tests
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

---
+++ tests/Railpage/Images/ImageTest.php
@@ -0,0 +1,25 @@
+<?php
+	
+	use Railpage\Images\Images;
+	use Railpage\Images\Image;
+	
+	class ImageTest extends PHPUnit_Framework_TestCase {
+		
+		public function testFetchFlickr() {
+			$Image = (new Images)->getImageFromUrl("https://www.flickr.com/photos/doctorjbeam/17309304565/");
+			
+			$this->assertEquals("Escale", $Image->title);
+			$this->assertEquals("flickr", $Image->provider);
+			$this->assertFalse(!filter_var($Image->id, FILTER_VALIDATE_INT)); 
+		}
+		
+		public function testFetchSmugMug() {
+			$Image = (new Images)->getImageFromUrl("http://sjbphotography.smugmug.com/Railways/The-Adventures/Epic-Adventures/The-International-Adventures/Steel-Steam-Stars-IV/i-7kHVHtX/A");
+			
+			$this->assertEquals("SmugMug", $Image->provider);
+			$this->assertEquals("7kHVHtX", $Image->photo_id); 
+			$this->assertTrue(count($Image->sizes) > 1);
+			$this->assertFalse(!filter_var($Image->id, FILTER_VALIDATE_INT)); 
+		}
+	}
+	
