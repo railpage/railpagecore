b5bb8db208d3373b696bf1180eea77f86d710dff
Michael Greenhill <doctorjbeam@gmail.com>
2015-07-27 08:24:37 +1000

    More statistics
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- lib/Images/Statistics.php
+++ lib/Images/Statistics.php
@@ -52,7 +52,15 @@
 				UNION SELECT "Photos photos of locomotives" AS title, FORMAT(COUNT(*), 0) AS num FROM image AS i LEFT JOIN image_link AS il ON i.id = il.image_id WHERE il.namespace = "railpage.locos.loco" AND ignored = 0
 				UNION SELECT "Photos photos of loco liveries" AS title, FORMAT(COUNT(*), 0) AS num FROM image AS i LEFT JOIN image_link AS il ON i.id = il.image_id WHERE il.namespace = "railpage.locos.liveries.livery" AND ignored = 0
 				UNION SELECT "Most photographed locomotive" AS title, l.loco_num AS num FROM loco_unit AS l LEFT JOIN loco_class AS c ON l.class_id = c.id WHERE l.loco_id = ( SELECT namespace_key FROM image_link WHERE namespace = "railpage.locos.loco" GROUP BY namespace_key ORDER BY COUNT(*) DESC, namespace_key LIMIT 0,1 )
-				UNION SELECT "Most photographed loco class" AS title, c.name AS num FROM loco_class AS c WHERE c.id = ( SELECT namespace_key FROM image_link WHERE namespace = "railpage.locos.class" GROUP BY namespace_key ORDER BY COUNT(*) DESC, namespace_key LIMIT 0,1 )';
+				UNION SELECT "Most photographed loco class" AS title, c.name AS num FROM loco_class AS c WHERE c.id = ( SELECT namespace_key FROM image_link WHERE namespace = "railpage.locos.class" GROUP BY namespace_key ORDER BY COUNT(*) DESC, namespace_key LIMIT 0,1 )
+				UNION SELECT * FROM (SELECT "Most popular camera" AS title, CONCAT(camera_make, " ", camera_model) AS num FROM (
+    SELECT DISTINCT e.camera_id, i.user_id, c.make AS camera_make, c.model AS camera_model
+    FROM image_exif AS e 
+    LEFT JOIN image_camera AS c ON c.id = e.camera_id
+    LEFT JOIN image AS i on e.image_id = i.id 
+    WHERE e.camera_id != 0 
+    AND i.user_id != 0
+) AS dist WHERE camera_make != "Unknown" GROUP BY camera_id ORDER BY COUNT(*) DESC LIMIT 0, 1) AS camera';
 			
 			return $this->db->fetchAll($query); 
 			
