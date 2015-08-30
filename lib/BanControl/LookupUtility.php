1cbfdc869795cb9efecc21255d89f7e7807570d5
Michael Greenhill <doctorjbeam@gmail.com>
2015-08-30 21:39:09 +1000

    bugfix
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- lib/BanControl/LookupUtility.php
+++ lib/BanControl/LookupUtility.php
@@ -69,6 +69,52 @@
 		}
 		
 		/**
+		 * Recent bans
+		 * @since Version 3.10.0
+		 * @return array
+		 */
+		
+		public static function recent($activeOnly = true) {
+			
+			$Database = (new AppCore)->getDatabaseConnection(); 
+			
+			$query = "SELECT b.id, b.user_id, un.username AS username, b.ban_active, b.ban_time, b.ban_expire, b.ban_reason, b.banned_by, u.username AS banned_by_username 
+					FROM bancontrol AS b 
+					LEFT JOIN nuke_users AS u ON b.banned_by = u.user_id 
+					LEFT JOIN nuke_users AS un ON b.user_id = un.user_id 
+					ORDER BY b.id DESC 
+					LIMIT 0, 10";
+
+			
+			$return = array(); 
+			
+			foreach ($Database->fetchAll($query) as $row) {
+				if ($activeOnly === false || ($activeOnly === true && $row['ban_active'] == 1)) {
+					if (!$row['ban_time'] instanceof DateTime) {
+						$row['ban_time'] = new DateTime("@" . $row['ban_time']); 
+					}
+					
+					$row['ban_time_nice'] = $row['ban_time']->format("F j, Y");
+					
+					if ($row['ban_expire'] > 0) {
+						if (!$row['ban_expire'] instanceof DateTime) {
+							$row['ban_expire'] = new DateTime("@" . $row['ban_expire']); 
+						}
+						
+						$row['ban_expire_nice'] = $row['ban_expire']->format("F j, Y");
+					} else {
+						$row['ban_expire'] = 0; 
+					}
+					
+					$return[$row['id']] = $row; 
+				}
+			}
+			
+			return $return;
+			
+		}
+		
+		/**
 		 * Check SpamCop for the given IP address
 		 * @since Version 3.10.0
 		 * @return boolean
