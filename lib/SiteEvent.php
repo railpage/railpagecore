27edab4dfd9fda5b22743c56f6290e5f84d00ad0
Michael Greenhill <doctorjbeam@gmail.com>
2016-02-10 15:20:57 +1100

    Change: line indentation
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

--- lib/SiteEvent.php
+++ lib/SiteEvent.php
@@ -1,201 +1,201 @@
 <?php
+
+/**
+ * Lot site events
+ * @package Railpage
+ * @since Version 3.6
+ * @author Michael Greenhill
+ */
+
+namespace Railpage;
+
+use Railpage\SiteEvent\Base;
+use Exception;
+use DateTime;
+use Railpage\Debug;
+use Railpage\Url;
+
+/**
+ * Show / create an event
+ */
+
+class SiteEvent extends Base {
+    
     /**
-     * Lot site events
-     * @package Railpage
+     * Event ID
      * @since Version 3.6
-     * @author Michael Greenhill
+     * @var int $id
      */
-    
-    namespace Railpage;
-    
-    use Railpage\SiteEvent\Base;
-    use Exception;
-    use DateTime;
-    use Railpage\Debug;
-    use Railpage\Url;
+     
+    public $id;
     
     /**
-     * Show / create an event
+     * Module name
+     * @since Version 3.6
+     * @var string $module_name
      */
     
-    class SiteEvent extends Base {
+    public $module_name;
+    
+    /**
+     * Event User ID
+     * @since Version 3.6
+     * @var int $user_id
+     */
+    
+    public $user_id;
+    
+    /**
+     * Event username
+     * @since Version 3.6
+     * @var string $username
+     */
+    
+    public $username;
+    
+    /**
+     * Timestamp
+     * @since Version 3.6
+     * @var object $timestamp
+     */
+    
+    public $timestamp; 
+    
+    /**
+     * Event title
+     * @since Version 3.6
+     * @var string $title
+     */
+    
+    public $title;
+    
+    /**
+     * Event args 
+     * @since Version 3.6
+     * @var array $args
+     */
+    
+    public $args;
+    
+    /**
+     * Event key
+     * @since Version 3.6
+     * @var mixed $key
+     */
+    
+    public $key;
+    
+    /**
+     * Event key value
+     * @since Version 3.6
+     * @var mixed $value
+     */
+    
+    public $value;
+    
+    /**
+     * Constructor
+     * @since Version 3.6
+     * @param int $event_id
+     */
+    
+    public function __construct($event_id = false) {
+        
+        parent::__construct(); 
         
         /**
-         * Event ID
-         * @since Version 3.6
-         * @var int $id
-         */
-         
-        public $id;
-        
-        /**
-         * Module name
-         * @since Version 3.6
-         * @var string $module_name
+         * Record this in the debug log
          */
         
-        public $module_name;
+        Debug::RecordInstance(); 
         
-        /**
-         * Event User ID
-         * @since Version 3.6
-         * @var int $user_id
-         */
-        
-        public $user_id;
-        
-        /**
-         * Event username
-         * @since Version 3.6
-         * @var string $username
-         */
-        
-        public $username;
-        
-        /**
-         * Timestamp
-         * @since Version 3.6
-         * @var object $timestamp
-         */
-        
-        public $timestamp; 
-        
-        /**
-         * Event title
-         * @since Version 3.6
-         * @var string $title
-         */
-        
-        public $title;
-        
-        /**
-         * Event args 
-         * @since Version 3.6
-         * @var array $args
-         */
-        
-        public $args;
-        
-        /**
-         * Event key
-         * @since Version 3.6
-         * @var mixed $key
-         */
-        
-        public $key;
-        
-        /**
-         * Event key value
-         * @since Version 3.6
-         * @var mixed $value
-         */
-        
-        public $value;
-        
-        /**
-         * Constructor
-         * @since Version 3.6
-         * @param int $event_id
-         */
-        
-        public function __construct($event_id = false) {
+        if ($event_id) {
+            $this->id = $event_id; 
             
-            parent::__construct(); 
-            
-            /**
-             * Record this in the debug log
-             */
-            
-            Debug::RecordInstance(); 
-            
-            if ($event_id) {
-                $this->id = $event_id; 
-                
-                $this->fetch(); 
-            }
-        }
-        
-        /**
-         * Fetch an event
-         * @since Version 3.6
-         * @return void
-         */
-        
-        private function fetch() {
-            
-            $query = "SELECT e.id, e.module, e.user_id, u.username, e.timestamp, e.title, e.args, e.key, e.value FROM log_general AS e INNER JOIN nuke_users AS u ON u.user_id = e.user_id WHERE e.id = ?"; 
-            
-            $row = $this->db->fetchRow($query, $this->id);
-            $this->module_name = $row['module'];
-            $this->user_id = $row['user_id'];
-            $this->username = $row['username'];
-            $this->timestamp = $row['timestamp'];
-            $this->title = $row['title'];
-            $this->args = json_decode($row['args'], true);
-            $this->key = $row['key'];
-            $this->value = $row['value'];
-            
-        }
-        
-        /**
-         * Validate changes to this event
-         * @since Version 3.6
-         * @return boolean
-         */
-        
-        public function validate() {
-            if (empty($this->title)) {
-                throw new Exception("Cannot validate site event - title cannot be empty!"); 
-                return false;
-            }
-            
-            if (empty($this->user_id)) {
-                throw new Exception("Cannot validate site event - user ID cannot be empty!"); 
-                return false;
-            }
-            
-            if (empty($this->key)) {
-                throw new Exception("Cannot validate site event - key cannot be empty!"); 
-                return false;
-            }
-            
-            if (empty($this->args)) {
-                $this->args = "";
-            }
-            
-            return true;
-        }
-        
-        /**
-         * Commit changes to an event
-         * @since Version 3.6
-         * @return boolean
-         */
-        
-        public function commit() {
-            $this->validate(); 
-            
-            $data = array(
-                "module" => $this->module_name,
-                "user_id" => $this->user_id,
-                "title" => $this->title,
-                "args" => is_array($this->args) ? json_encode($this->args) : $this->args,
-                "key" => $this->key,
-                "value" => $this->value
-            );
-            
-            if (filter_var($this->id, FILTER_VALIDATE_INT)) {
-                $where = array(
-                    "id = ?" => $this->id
-                );
-                
-                $this->db->update("log_general", $data, $where);
-            } else {
-                $this->db->insert("log_general", $data);
-                $this->id = $this->db->lastInsertId();
-            }
-            
-            return true;
+            $this->fetch(); 
         }
     }
     
+    /**
+     * Fetch an event
+     * @since Version 3.6
+     * @return void
+     */
+    
+    private function fetch() {
+        
+        $query = "SELECT e.id, e.module, e.user_id, u.username, e.timestamp, e.title, e.args, e.key, e.value FROM log_general AS e INNER JOIN nuke_users AS u ON u.user_id = e.user_id WHERE e.id = ?"; 
+        
+        $row = $this->db->fetchRow($query, $this->id);
+        $this->module_name = $row['module'];
+        $this->user_id = $row['user_id'];
+        $this->username = $row['username'];
+        $this->timestamp = $row['timestamp'];
+        $this->title = $row['title'];
+        $this->args = json_decode($row['args'], true);
+        $this->key = $row['key'];
+        $this->value = $row['value'];
+        
+    }
+    
+    /**
+     * Validate changes to this event
+     * @since Version 3.6
+     * @return boolean
+     */
+    
+    public function validate() {
+        if (empty($this->title)) {
+            throw new Exception("Cannot validate site event - title cannot be empty!"); 
+            return false;
+        }
+        
+        if (empty($this->user_id)) {
+            throw new Exception("Cannot validate site event - user ID cannot be empty!"); 
+            return false;
+        }
+        
+        if (empty($this->key)) {
+            throw new Exception("Cannot validate site event - key cannot be empty!"); 
+            return false;
+        }
+        
+        if (empty($this->args)) {
+            $this->args = "";
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Commit changes to an event
+     * @since Version 3.6
+     * @return boolean
+     */
+    
+    public function commit() {
+        $this->validate(); 
+        
+        $data = array(
+            "module" => $this->module_name,
+            "user_id" => $this->user_id,
+            "title" => $this->title,
+            "args" => is_array($this->args) ? json_encode($this->args) : $this->args,
+            "key" => $this->key,
+            "value" => $this->value
+        );
+        
+        if (filter_var($this->id, FILTER_VALIDATE_INT)) {
+            $where = array(
+                "id = ?" => $this->id
+            );
+            
+            $this->db->update("log_general", $data, $where);
+        } else {
+            $this->db->insert("log_general", $data);
+            $this->id = $this->db->lastInsertId();
+        }
+        
+        return true;
+    }
+}
--- lib/Submenu.php
+++ lib/Submenu.php
@@ -1,246 +1,246 @@
 <?php
+
+/**
+ * Construct a submenu for a page
+ * @since Version 3.4
+ * @package Railpage
+ * @author Michael Greenhill
+ */
+
+namespace Railpage; 
+
+use Railpage\AppCore;
+use Railpage\Debug;
+use Railpage\Url;
+use Exception;
+use InvalidArgumentException;
+
+/**
+ * Submenu class
+ * @since Version 3.4
+ */
+
+class Submenu extends AppCore {
+    
     /**
-     * Construct a submenu for a page
+     * Menu array
      * @since Version 3.4
-     * @package Railpage
-     * @author Michael Greenhill
+     * @var array $menu
      */
     
-    namespace Railpage; 
-    
-    use Railpage\AppCore;
-    use Railpage\Debug;
-    use Railpage\Url;
-    use Exception;
-    use InvalidArgumentException;
+    public $menu;
     
     /**
-     * Submenu class
+     * The internal pointer for the submenu section we're defaulting to
+     * @since Version 3.9.1
+     * @var string $section
+     */
+    
+    public $section;
+    
+    /**
+     * Constructor
      * @since Version 3.4
      */
     
-    class Submenu extends AppCore {
+    public function __construct() {
+        // Placeholder - do nothing for now
         
-        /**
-         * Menu array
-         * @since Version 3.4
-         * @var array $menu
-         */
+        Debug::RecordInstance(); 
         
-        public $menu;
-        
-        /**
-         * The internal pointer for the submenu section we're defaulting to
-         * @since Version 3.9.1
-         * @var string $section
-         */
-        
-        public $section;
-        
-        /**
-         * Constructor
-         * @since Version 3.4
-         */
-        
-        public function __construct() {
-            // Placeholder - do nothing for now
-            
-            Debug::RecordInstance(); 
-            
+    }
+    
+    /**
+     * Add a menu grouping
+     * @since Version 3.8.7
+     * @param string $title The title of this submenu category
+     * @param string $subtitle A subtitle to apply to this category
+     */
+    
+    public function AddGrouping($title = false, $subtitle = NULL) {
+        if (empty(filter_var($title, FILTER_SANITIZE_STRING))) {
+            throw new InvalidArgumentException("Cannot add menu grouping - no title given"); 
         }
         
-        /**
-         * Add a menu grouping
-         * @since Version 3.8.7
-         * @param string $title The title of this submenu category
-         * @param string $subtitle A subtitle to apply to this category
-         */
-        
-        public function AddGrouping($title = false, $subtitle = NULL) {
-            if (empty(filter_var($title, FILTER_SANITIZE_STRING))) {
-                throw new InvalidArgumentException("Cannot add menu grouping - no title given"); 
-            }
-            
-            if (!isset($this->menu[$title])) {
-                $this->menu[$title] = array(
-                    "title" => $title,
-                    "subtitle" => $subtitle,
-                    "menu" => array()
-                );
-            }
-            
-            #$this->section = $title;
-            
-            return $this;
+        if (!isset($this->menu[$title])) {
+            $this->menu[$title] = array(
+                "title" => $title,
+                "subtitle" => $subtitle,
+                "menu" => array()
+            );
         }
         
-        /**
-         * Set subtitle for a grouping
-         * @since Version 3.8.7
-         * @param string $grouping The title of the grouping/category to change
-         * @param string $subtitle The subtitle to apply to ths grouping/category
-         */
+        #$this->section = $title;
         
-        public function SetGroupingSubtitle($grouping = false, $subtitle = false) {
-            if ($grouping && $subtitle) {
-                if (isset($this->menu[$grouping])) {
-                    $this->menu[$grouping]['subtitle'] = $subtitle;
-                }
+        return $this;
+    }
+    
+    /**
+     * Set subtitle for a grouping
+     * @since Version 3.8.7
+     * @param string $grouping The title of the grouping/category to change
+     * @param string $subtitle The subtitle to apply to ths grouping/category
+     */
+    
+    public function SetGroupingSubtitle($grouping = false, $subtitle = false) {
+        if ($grouping && $subtitle) {
+            if (isset($this->menu[$grouping])) {
+                $this->menu[$grouping]['subtitle'] = $subtitle;
             }
-        }
-        
-        /**
-         * Add menu entry
-         * @since Version 3.4
-         * @param string $title The title of the menu item
-         * @param string $url A link to apply
-         * @param string $grouping An optional grouping/category this menu item belongs to
-         * @param array $meta An optional array of parameters to apply to this menu item
-         */
-        
-        public function Add($title = false, $url = false, $grouping = false, $meta = false) {
-            if (empty(filter_var($title, FILTER_SANITIZE_STRING))) {
-                throw new InvalidArgumentException("Cannot add item to menu - no title given"); 
-            }
-            
-            $i = count($this->menu); 
-            
-            if ($grouping || (isset($this->section) && !empty($this->section))) {
-                
-                if (isset($this->section) && !empty($this->section)) {
-                    $grouping = $this->section;
-                }
-                
-                $this->AddGrouping($grouping);
-                
-                $i = count($this->menu[$grouping]['menu']); 
-                
-                // Check if this already exists
-                foreach ($this->menu[$grouping]['menu'] as $k => $menu) {
-                    if ($menu['title'] == $title) {
-                        $i = $k;
-                        break;
-                    }
-                }
-                
-                $this->menu[$grouping]['menu'][$i]['title'] = $title; 
-                
-                if ($url) {
-                    $this->menu[$grouping]['menu'][$i]['url'] = $url;
-                }
-                
-                if ($meta) {
-                    $this->menu[$grouping]['menu'][$i]['meta'] = $meta;
-                }
-            } else {
-                $this->menu[$i]['title'] = $title; 
-                
-                if ($url) {
-                    $this->menu[$i]['url'] = $url;
-                }
-                
-                if ($meta) {
-                    $this->menu[$i]['meta'] = $meta;
-                }
-            }
-            
-            return $this;
-        }
-        
-        /**
-         * Get the URL for a menu item
-         * @param string $title
-         * @return string
-         */
-        
-        public function GetURL($title = false) {
-            if (empty(filter_var($title, FILTER_SANITIZE_STRING))) {
-                throw new InvalidArgumentException("Cannot return submenu URL - no title provided to look for");
-            }
-            
-            foreach ($this->menu as $key => $data) {
-                if ($data['title'] == $title) {
-                    return $data['url'];
-                }
-            }
-            
-            return false;
-        }
-        
-        /**
-         * Get menu as HTML
-         * @return string
-         */
-        
-        public function GetHTML() {
-            $Smarty = AppCore::GetSmarty(); 
-            
-            $Smarty->Assign("submenu", $this->menu);
-            return $Smarty->Fetch(RP_SITE_ROOT . DS . "content" . DS . "inc.submenu.tpl");
-        }
-        
-        /**
-         * Set the section
-         * @since Version 3.8.7
-         * @param string $section
-         */
-        
-        public function Section($title = NULL) {
-            if (isset($this->menu[$title])) {
-                $this->section = $title;
-            } else {
-                $this->section = $title;
-                $this->AddGrouping($title);
-            }
-            
-            return $this;
-        }
-        
-        /**
-         * Add a section but don't switch to it
-         * @since Version 3.8.7
-         * @param string $section
-         */
-        
-        public function AddSection($title = NULL) {
-            return $this->AddGrouping($title);
-        }
-        
-        /**
-         * Check if there's anything in this menu
-         * @since Version 3.9
-         * @return boolean
-         */
-        
-        public function HasItems() {
-            if (!is_array($this->menu) || count($this->menu) === 0) {
-                return false;
-            }
-            
-            foreach ($this->menu as $section) {
-                if (isset($section['menu']) && count($section['menu'])) {
-                    return true;
-                }
-            }
-            
-            return false;
-        }
-        
-        /**
-         * Add arbitrary HTML to the submenu
-         * @since Version 3.9.1
-         * @param string $html
-         * @return void
-         */
-        
-        public function AddHTML($html) {
-            
-            if (isset($this->section) && !empty($this->section)) {
-                $this->menu[$this->section]['html'][] = $html;
-            } else {
-                $this->menu['html'][] = $html;
-            }
-            
         }
     }
     
+    /**
+     * Add menu entry
+     * @since Version 3.4
+     * @param string $title The title of the menu item
+     * @param string $url A link to apply
+     * @param string $grouping An optional grouping/category this menu item belongs to
+     * @param array $meta An optional array of parameters to apply to this menu item
+     */
+    
+    public function Add($title = false, $url = false, $grouping = false, $meta = false) {
+        if (empty(filter_var($title, FILTER_SANITIZE_STRING))) {
+            throw new InvalidArgumentException("Cannot add item to menu - no title given"); 
+        }
+        
+        $i = count($this->menu); 
+        
+        if ($grouping || (isset($this->section) && !empty($this->section))) {
+            
+            if (isset($this->section) && !empty($this->section)) {
+                $grouping = $this->section;
+            }
+            
+            $this->AddGrouping($grouping);
+            
+            $i = count($this->menu[$grouping]['menu']); 
+            
+            // Check if this already exists
+            foreach ($this->menu[$grouping]['menu'] as $k => $menu) {
+                if ($menu['title'] == $title) {
+                    $i = $k;
+                    break;
+                }
+            }
+            
+            $this->menu[$grouping]['menu'][$i]['title'] = $title; 
+            
+            if ($url) {
+                $this->menu[$grouping]['menu'][$i]['url'] = $url;
+            }
+            
+            if ($meta) {
+                $this->menu[$grouping]['menu'][$i]['meta'] = $meta;
+            }
+        } else {
+            $this->menu[$i]['title'] = $title; 
+            
+            if ($url) {
+                $this->menu[$i]['url'] = $url;
+            }
+            
+            if ($meta) {
+                $this->menu[$i]['meta'] = $meta;
+            }
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Get the URL for a menu item
+     * @param string $title
+     * @return string
+     */
+    
+    public function GetURL($title = false) {
+        if (empty(filter_var($title, FILTER_SANITIZE_STRING))) {
+            throw new InvalidArgumentException("Cannot return submenu URL - no title provided to look for");
+        }
+        
+        foreach ($this->menu as $key => $data) {
+            if ($data['title'] == $title) {
+                return $data['url'];
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Get menu as HTML
+     * @return string
+     */
+    
+    public function GetHTML() {
+        $Smarty = AppCore::GetSmarty(); 
+        
+        $Smarty->Assign("submenu", $this->menu);
+        return $Smarty->Fetch(RP_SITE_ROOT . DS . "content" . DS . "inc.submenu.tpl");
+    }
+    
+    /**
+     * Set the section
+     * @since Version 3.8.7
+     * @param string $section
+     */
+    
+    public function Section($title = NULL) {
+        if (isset($this->menu[$title])) {
+            $this->section = $title;
+        } else {
+            $this->section = $title;
+            $this->AddGrouping($title);
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Add a section but don't switch to it
+     * @since Version 3.8.7
+     * @param string $section
+     */
+    
+    public function AddSection($title = NULL) {
+        return $this->AddGrouping($title);
+    }
+    
+    /**
+     * Check if there's anything in this menu
+     * @since Version 3.9
+     * @return boolean
+     */
+    
+    public function HasItems() {
+        if (!is_array($this->menu) || count($this->menu) === 0) {
+            return false;
+        }
+        
+        foreach ($this->menu as $section) {
+            if (isset($section['menu']) && count($section['menu'])) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Add arbitrary HTML to the submenu
+     * @since Version 3.9.1
+     * @param string $html
+     * @return void
+     */
+    
+    public function AddHTML($html) {
+        
+        if (isset($this->section) && !empty($this->section)) {
+            $this->menu[$this->section]['html'][] = $html;
+        } else {
+            $this->menu['html'][] = $html;
+        }
+        
+    }
+}
